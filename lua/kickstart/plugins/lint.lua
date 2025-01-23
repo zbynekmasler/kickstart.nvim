2542fff767df5bdff88bfd76acd4a6c294704757
Zbynek Masler <zbynekmasler@gmail.com>
2025-01-23 21:49:31 +0000

    add: pyflake8 into linters

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -7,6 +7,10 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
+        python = {
+          command = 'flake8',
+          args = { '--max-line-length=100' },
+        },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
